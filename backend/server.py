7b48a1495ed662a2120ecf98baa8b7f298aa8d0e
Elroi Luria <elroi.luria@gmail.com>
2019-07-17 19:55:05 +0300

    change db table Groups to User_Groups due to saved word use

--- backend/server.py
+++ backend/server.py
@@ -1,31 +1,37 @@
-from bottle import run, post
+from bottle import run, route, post
+
 
 # Frontend
+
+@route('/')
+def homepage():
+    print('hellllloooo nurse')
+    return 'hellllloooo nurse'
+
+
 
 # Update location
 @post('/update_user_location')
 def update_user_location():
     print('hello update_user_location')
+    return('hello update_user_location')
+
 
 # Report
 @post('/create_user_report')
 def create_user_report():
     print('hello create_user_report')
+    return('hello create_user_report')
+
 
 # Alert
 @post('/create_user_report')
 def update_user_location():
     print('hello update_user_location')
+    return('hello update_user_location')
+
 
 # Backend
 
 
-
-run(host='localhost',port=4261,reloader=True,debug=True)
-
-
-
-
-
-
-
+run(host='localhost', port=4261, reloader=True, debug=True)
