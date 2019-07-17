4649f1e0c7e481831f93d8f05d58b257de4fbc1a
Elroi Luria <elroi.luria@gmail.com>
2019-07-17 16:48:23 +0300

    init backend

---
+++ backend/server.py
@@ -0,0 +1,18 @@
+from bottle import run
+
+run(host='localhost',port=4261,reloader=True,debug=True)
+
+# Frontend
+
+# Update location
+# @route('/update_location')
+
+# Report
+# -
+# @route('/report')
+
+
+# Alert
+
+
+# Backend
